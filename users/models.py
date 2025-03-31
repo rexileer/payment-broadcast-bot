9523b67471cb0053d7e53c9550c53d967c2c65e7
rexileer <110720074+rexileer@users.noreply.github.com>
2025-03-31 15:39:29 +0300

    Users

--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    telegram_id = models.BigIntegerField(unique=True)
+    is_active = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    subscription_until = models.DateTimeField(null=True, blank=True)
+    
+    class Meta:
+        verbose_name = 'Пользователь telegram'
+        verbose_name_plural = 'Пользователи telegram'
