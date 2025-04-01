302e5129a9d830722db587e32358bcbf7b719df2
rexileer <110720074+rexileer@users.noreply.github.com>
2025-04-01 11:29:25 +0300

    Update config.py
    session

--- bot/config.py
+++ bot/config.py
@@ -1,6 +1,8 @@
 from django.conf import settings
 from aiogram import Bot
 from aiogram.client.bot import DefaultBotProperties
+from aiogram.client.session.aiohttp import AiohttpSession
 
 TOKEN = settings.TELEGRAM_BOT_TOKEN
-bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode='HTML'))
+session = AiohttpSession()
+bot = Bot(token=TOKEN, session=session, default=DefaultBotProperties(parse_mode='HTML'))
