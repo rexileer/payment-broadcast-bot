076954ad5454f7e5bd9f17fbbb990051ec09fbbe
rexileer <110720074+rexileer@users.noreply.github.com>
2025-04-01 17:31:40 +0300

    rename sender to post_sender

--- posting/signals.py
+++ posting/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import PostingMessage
-from bot.sender import send_posting
+from bot.post_sender import send_posting
 
 @receiver(post_save, sender=PostingMessage)
 def send_posting_on_create(sender, instance, created, **kwargs):
