f2c970ed7027e4960c5e61e14fdc4f9675f32261
rexileer <110720074+rexileer@users.noreply.github.com>
2025-04-09 14:37:48 +0300

    Create create_session.py

---
+++ create_session.py
@@ -0,0 +1,14 @@
+from pyrogram import Client
+import os
+from dotenv import load_dotenv
+
+load_dotenv(encoding='utf-8')
+
+API_ID = os.getenv('API_ID')
+API_HASH = os.getenv('API_HASH')
+
+session_path = "bot/s1" 
+
+app = Client(session_path, api_id=API_ID, api_hash=API_HASH)
+
+app.run()  # Запустит клиент, откроет ввод кода авторизации
